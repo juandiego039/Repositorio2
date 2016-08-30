d93e62015a587deb321092ae707151a7c78876ac
jdrojasz <jdrojasz@unal.edu.co>
2016-08-29 20:30:00 -0500

    Comentarios de las variables

--- Simulacion.cpp
+++ Simulacion.cpp
@@ -28,13 +28,16 @@ int fragmentar(int s[], int u, int j)
 
 int main(void)
 {
-	int N = 100;                   
-	double nu = 0.01;              
-	int Num_iteraciones =1000;     
+	/* *** *** Entradas del programa *** *** */
+	int N = 100;                   //Total de fuerza de ataque del ejercito
+	double nu = 0.01;              //Probabilidad de que la unidad se fragmente
+	int Num_iteraciones =1000;     //Se define el número de iteraciones
 		
+	/* *** *** Definición de variables *** *** */
+	int s[N];					   //Vector de fuerza
+	s[0] = N;					   //Valor inicial como un ejercito unido
+	for(int i=1;i<N;i++){s[i]=0;}  //Las casillas sobrantes se llenan con 0
+	int u = 1;                     //Número de unidades 1<= u <= N
 	
-	int s[N];					   
-	s[0] = N;					   
-	for(int i=1;i<N;i++){s[i]=0;}  
 	return 0;
 }
